178b3ded6a13ef4395621615d3168bd126b6f425
Thomas Knoefel <tknoefel@yahoo.de>
2023-07-10 21:54:17 +0200

    Removed NPP prefix from files and repository; project name changes reflected in build output

--- src/PluginDefinition.cpp
+++ src/PluginDefinition.cpp
@@ -16,7 +16,7 @@
 //Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
 #include "PluginDefinition.h"
-#include "MultiReplace.h"
+#include "MultiReplacePanel.h"
 #include "AboutDialog.h"
 
 
