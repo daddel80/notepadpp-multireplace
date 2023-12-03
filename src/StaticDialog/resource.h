e05e8c7962b8a205ff4a4600807ce844d887d2b7
Thomas Knoefel <tknoefel@yahoo.de>
2023-12-03 11:38:41 +0100

    dropped  help_use_variables.html in src directory; corrected TAB order

--- src/StaticDialog/resource.h
+++ src/StaticDialog/resource.h
@@ -28,17 +28,18 @@
 #define IDC_REPLACE_EDIT                5002
 #define IDC_SWAP_BUTTON					5003
 #define IDC_COPY_TO_LIST_BUTTON         5004
-#define IDC_REPLACE_ALL_BUTTON          5005
-#define IDC_REPLACE_DROPDOWN_BUTTON     5006
-#define IDC_REPLACE_BUTTON              5007
-#define IDC_REPLACE_ALL_SMALL_BUTTON    5008
-#define IDC_2_BUTTONS_MODE              5009
-#define IDC_FIND_BUTTON                 5010
-#define IDC_FIND_NEXT_BUTTON            5011
-#define IDC_FIND_PREV_BUTTON            5012
-#define IDC_MARK_BUTTON                 5013
-#define IDC_MARK_MATCHES_BUTTON         5014
-#define IDC_CLEAR_MARKS_BUTTON          5015
+#define IDC_USE_LIST_CHECKBOX			5005
+#define IDC_REPLACE_ALL_BUTTON          5006
+#define IDC_REPLACE_DROPDOWN_BUTTON     5007
+#define IDC_REPLACE_BUTTON              5008
+#define IDC_REPLACE_ALL_SMALL_BUTTON    5009
+#define IDC_2_BUTTONS_MODE              5010
+#define IDC_FIND_BUTTON                 5011
+#define IDC_FIND_NEXT_BUTTON            5012
+#define IDC_FIND_PREV_BUTTON            5013
+#define IDC_MARK_BUTTON                 5014
+#define IDC_MARK_MATCHES_BUTTON         5015
+#define IDC_CLEAR_MARKS_BUTTON          5016
 #define IDC_COPY_MARKED_TEXT_BUTTON     5017
 #define IDC_REPLACE_ALL_IN_LIST_BUTTON  5018
 #define IDC_DELETE_REPLACE_ITEM_BUTTON  5019
@@ -51,7 +52,6 @@
 #define IDC_SHIFT_TEXT					5026
 #define ID_REPLACE_ALL_OPTION           5027
 #define ID_REPLACE_IN_ALL_DOCS_OPTION   5028
-#define IDC_USE_VARIABLES_HELP          5029
 
 #define IDC_STATIC_FIND                 5100
 #define IDC_STATIC_REPLACE              5101
@@ -60,8 +60,9 @@
 #define IDC_WHOLE_WORD_CHECKBOX         5200
 #define IDC_MATCH_CASE_CHECKBOX         5201
 #define IDC_USE_VARIABLES_CHECKBOX      5202
-#define IDC_REPLACE_FIRST_CHECKBOX      5203
-#define IDC_WRAP_AROUND_CHECKBOX        5204
+#define IDC_USE_VARIABLES_HELP          5203
+#define IDC_REPLACE_FIRST_CHECKBOX      5204
+#define IDC_WRAP_AROUND_CHECKBOX        5205
 
 #define IDC_SEARCH_MODE_GROUP           5300
 #define IDC_NORMAL_RADIO                5301
@@ -72,16 +73,15 @@
 #define IDC_ALL_TEXT_RADIO              5452
 #define IDC_SELECTION_RADIO             5453
 #define IDC_COLUMN_MODE_RADIO           5454
-#define IDC_COLUMN_NUM_EDIT             5455
-#define IDC_DELIMITER_EDIT              5456
-#define IDC_QUOTECHAR_EDIT              5457
-#define IDC_COLUMN_HIGHLIGHT_BUTTON     5458
+#define IDC_COLUMN_HIGHLIGHT_BUTTON     5455
+#define IDC_COLUMN_NUM_EDIT             5456
+#define IDC_DELIMITER_EDIT              5457
+#define IDC_QUOTECHAR_EDIT              5458
 #define IDC_DELIMITER_STATIC            5459
 #define IDC_COLUMN_NUM_STATIC           5460
 #define IDC_QUOTECHAR_STATIC            5461
 
 #define IDC_STATIC_FRAME                5501
-#define IDC_USE_LIST_CHECKBOX			5502
 #define IDC_REPLACE_LIST                5503
 
 #define IDD_ABOUT_DIALOG                5600
@@ -106,7 +106,7 @@
 #define MIN_WIDTH 993
 #define MIN_HEIGHT 485
 
-#define IDC_WEBSITE_LINK_VALUE TEXT("https://github.com/daddel80/notepadpp-multireplace")
+#define IDC_WEBSITE_LINK_VALUE TEXT("https://github.com/daddel80/notepadpp-multireplace/issues")
 
 #endif // RESOURCE_H
 
