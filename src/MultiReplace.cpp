23e2ed3ea21ff4157246effafcac9a5a657009ee
Thomas Knoefel <tknoefel@yahoo.de>
2024-01-14 20:09:22 +0100

    changed funcItem to 0 -> fixed Icon problem

--- src/MultiReplace.cpp
+++ src/MultiReplace.cpp
@@ -80,7 +80,7 @@ extern "C" __declspec(dllexport) void beNotified(SCNotification * notifyCode)
         tbIcons.hToolbarBmp = ::LoadBitmap(g_inst, MAKEINTRESOURCE(IDR_SMILEY_BMP));
         tbIcons.hToolbarIcon = ::LoadIcon(g_inst, MAKEINTRESOURCE(IDI_SMILEY_ICON));
         tbIcons.hToolbarIconDarkMode = ::LoadIcon(g_inst, MAKEINTRESOURCE(IDI_SMILEY_DM_ICON));
-        ::SendMessage(nppData._nppHandle, NPPM_ADDTOOLBARICON_FORDARKMODE, funcItem[1]._cmdID, (LPARAM)&tbIcons);
+        ::SendMessage(nppData._nppHandle, NPPM_ADDTOOLBARICON_FORDARKMODE, funcItem[0]._cmdID, (LPARAM)&tbIcons);
     }
     break;
     case NPPN_SHUTDOWN:
